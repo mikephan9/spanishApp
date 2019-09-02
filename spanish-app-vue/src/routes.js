35d1a897fa80f925da24b0b39330647f74111e37
Michal-Han <han.michal@gmail.com>
2019-09-02 19:03:32 +0200

    adding components

---
+++ spanish-app-vue/src/routes.js
@@ -0,0 +1,7 @@
+import Home from './components/Home.vue';
+import Learn from './components/study/Learn.vue';
+
+export const routes = [
+    { path: '', component: Home },
+    { path: '/learn', component: Learn },
+];
